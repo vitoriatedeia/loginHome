6f2d9aac1a217626eec56d148d5631bc86f3aff8
Vitória Tedeia <163847549+vitoriatedeia@users.noreply.github.com>
2025-09-10 07:46:33 -0300

    commit

--- App.js
+++ App.js
@@ -1,15 +1,12 @@
 import React from "react";
 
 // Stack Navigation
-import { NavigationContainer } from "@react-navigation/native"; // Importa o componente NavigationContainer de "@react-navigation/native" para envolver a navegação da aplicação.
-import { createNativeStackNavigator } from "@react-navigation/native-stack"; // Importa a função createNativeStackNavigator de "@react-navigation/native-stack" para criar um navegador de pilha nativo.
-
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 // Páginas
 import Home from "./src/pages/HomeScreen";
 import Login from "./src/pages/LoginScreen";
 
-// Cria uma instância de um navegador de pilha usando createNativeStackNavigator e a armazena na constante Stack.
-// Isso permite que você use esse navegador de pilha para definir a navegação em sua aplicação.
 const Stack = createNativeStackNavigator();
 
 export default function App() {
